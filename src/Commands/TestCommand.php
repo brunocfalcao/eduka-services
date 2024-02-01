46b1bac6ae6d7a27f856f1f6b50a2703e884ffed
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-02-01 16:09:43 +0100

    Update TestCommand.php

--- src/Commands/TestCommand.php
+++ src/Commands/TestCommand.php
@@ -2,11 +2,8 @@
 
 namespace Eduka\Services\Commands;
 
-use Brunocfalcao\VimeoClient\Facades\VimeoClient;
 use Eduka\Abstracts\Classes\EdukaCommand;
-use Eduka\Cube\Models\Chapter;
-use Eduka\Cube\Models\Course;
-use Illuminate\Support\Str;
+use Eduka\Cube\Models\User;
 
 class TestCommand extends EdukaCommand
 {
@@ -21,66 +18,11 @@ class TestCommand extends EdukaCommand
 
     public function handle()
     {
-        /*
-        $course = Course::all()->first();
-        $course->name = Str::random(20);
-        $course->save();
-        */
+        // Let's delete my test user so we can use it for testing purposes.
+        User::where('email', env('MN_OR_TEST_EMAIL'))->first()
+            ->videosThatWereCompleted()
+            ->detach();
 
-        $chapter = Chapter::all()->first();
-        $chapter->name = Str::random(20);
-        $chapter->save();
-
-        $this->info('Ok.');
-
-        /*
-        $videoId = VimeoClient::upload(
-            resource_path('file-1.mp4'),
-            [
-                'name' => 'This is a name',
-                'description' => 'This is a description',
-                'privacy' => [
-                    'comments' => 'nobody',
-                    'embed' => 'whitelist',
-                    'view' => 'unlisted',
-                    'download' => true
-                 ],
-                'embed_domains' => [
-                    'brunofalcao.dev', 'masteringnova.com'
-                ]
-            ]
-        );
-        */
-
-        //dd(VimeoClient::request('/users/{user_id}/folders'));
-
-        //$course = Course::find(1)->first();
-
-        //dd(VimeoClient::upsertFolder('Laraning new videos', null, '47f269a6af138a7fad2d4962596c3407479c7bc9'));
-
-        /**
-        $folder = VimeoClient::upsertFolder('test');
-
-        foreach ($folder['body']['data'] as $item) {
-            $this->info($item['name'] . ' - ' . $item['resource_key']);
-        }
-
-        VimeoClient::upsertFolder('New Laraning Videos', '47f269a6af138a7fad2d4962596c3407479c7bc9');
-         **/
-        //dd($folder['body']['data'][0]);
-
-        //$course->update(['vimeo_folder_uri' => $folder['body']['uri']]);
-
-        //dd(array_keys($folder['body']['uri']));
-
-        //$this->info(json_encode(VimeoClient::getAllFolders()));
-
-        //$uri = VimeoClient::createPath('roger/that');
-
-        //$this->paragraph('video id: ' . $videoId);
-
-        //$this->info('All good.');
-
-        return 0;
+        User::where('email', env('MN_OR_TEST_EMAIL'))->forceDelete();
     }
 }
